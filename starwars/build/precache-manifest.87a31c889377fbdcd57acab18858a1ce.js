61426bca1d4cc68193dc82246f9ed43ed3a45dc1
b000rg <33711099+b000rg@users.noreply.github.com>
2020-08-31 20:04:25 -0500

    added node files

---
+++ starwars/build/precache-manifest.87a31c889377fbdcd57acab18858a1ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f625bd443425d57afa7143b08c5947a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ba665380ea9d29c81f5",
+    "url": "/static/css/main.77883493.chunk.css"
+  },
+  {
+    "revision": "17b635b6d8574cc3227b",
+    "url": "/static/js/2.1dc630a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1dc630a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba665380ea9d29c81f5",
+    "url": "/static/js/main.46e1dc10.chunk.js"
+  },
+  {
+    "revision": "a2ce235db025746ced47",
+    "url": "/static/js/runtime-main.42cd862b.js"
+  },
+  {
+    "revision": "593cf39d126a9d29f24f8cae44f36529",
+    "url": "/static/media/arrow.593cf39d.svg"
+  },
+  {
+    "revision": "8fb7702c55568cb5513d01538ccb2682",
+    "url": "/static/media/pk-bg.8fb7702c.jpg"
+  }
+]);
